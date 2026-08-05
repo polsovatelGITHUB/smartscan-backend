10c9d064e5e3fe6a4a664a273934cda1fdaa3dee
Meow <moiaeda75@gmail.com>
2026-08-06 00:27:33 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -92,13 +92,13 @@ app.post('/api/upload', upload.single('document'), async (req, res) => {
         console.log("Отправляем запрос к Gemini...");
        
         const response = await ai.models.generateContent({
-    model: 'gemini-1.5-flash', 
-    contents: [prompt, imagePart],
-            config: {
-                responseMimeType: "application/json",
-                temperature: 0.0,
-            }
-        });
+            model: 'gemini-1.5-flash-002', 
+            contents: [prompt, imagePart],
+            config: {
+                responseMimeType: "application/json",
+                temperature: 0.0,
+            }
+        });
 
         const aiText = response.text;
            
